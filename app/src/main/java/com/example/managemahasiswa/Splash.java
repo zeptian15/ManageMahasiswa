5b3e322787bd2eb7da3e745b34b609d78c13c28c
Zeptian <zeptian15@gmail.com>
2019-09-11 08:29:08 +0700

    Menambahkan beberapa file untuk Database

--- app/src/main/java/com/example/managemahasiswa/Splash.java
+++ app/src/main/java/com/example/managemahasiswa/Splash.java
@@ -2,13 +2,29 @@ package com.example.managemahasiswa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class Splash extends AppCompatActivity {
+
+    private int waktu_loading = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent dashboard = new Intent(Splash.this, DashboardActivity.class);
+                startActivity(dashboard);
+                finish();
+            }
+        },waktu_loading);
     }
 }
