cee249881acfcd0a8035cdc47243af4344b08d0e
yasfa <yasfazamzami0@Gmail.com>
2019-09-11 08:55:39 +0700

    Nambahin Insert,dan Update (Database Helper)

--- app/src/main/java/com/example/managemahasiswa/DashboardActivity.java
+++ app/src/main/java/com/example/managemahasiswa/DashboardActivity.java
@@ -2,13 +2,30 @@ package com.example.managemahasiswa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    Button inputData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        inputData = (Button) findViewById(R.id.inputdata);
+
+        inputData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent lihatData = new Intent(DashboardActivity.this,InputActivity.class);
+                startActivity(lihatData);
+            }
+        });
+
+
     }
 }
